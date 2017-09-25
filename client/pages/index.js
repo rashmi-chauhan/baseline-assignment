44760abfa6012825d3c7fb99f92ee96df04fd7da
Eric Nograles <grales@gmail.com>
2017-09-25 11:03:27 -0400

    LDS default

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import * as profileActionCreators from '../redux/actions/profile';
-import stylesheet from '../styles/index.scss';
+import stylesheet from '../styles/index.css';
 import { connectAsAnonymous } from '../redux/connectors';
 
 class PageLanding extends React.Component {
