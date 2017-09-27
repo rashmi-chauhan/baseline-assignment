f91c3067aa8cb83048e1ef35eed14c8f2d809ac9
Eric Nograles <grales@gmail.com>
2017-09-27 15:43:06 -0400

    dockerizes development

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM node:8.4.0
-ENV NODE_ENV production
+ENV NODE_ENV dev
 WORKDIR /app
 COPY . .
 RUN npm install -g yarn
-RUN yarn install
-EXPOSE 3000
+EXPOSE 3000 3001 5858
 VOLUME [ "/app" ]
 RUN echo "Successfully built ads-webapp-baseline"
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 // Server-wide settings
 winston.level = process.env.WINSTON_LEVEL || 'debug';
 const dev = process.env.NODE_ENV !== 'production';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Scaffold the server
 async function startServer() {
