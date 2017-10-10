114bf2e9017d60bf1eafb5833010b311fc32e8aa
Eric Nograles <grales@gmail.com>
2017-10-10 10:45:40 -0400

    coerces REDIS_REFRESH_TOKENS_DB value

--- api/services/constants.js
+++ api/services/constants.js
@@ -2,6 +2,6 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
   REDIS: {
     URL: process.env.REDIS_URL,
-    REFRESH_TOKENS_DB: process.env.REDIS_REFRESH_TOKENS_DB
+    REFRESH_TOKENS_DB: process.env.REDIS_REFRESH_TOKENS_DB || 1
   }
 };
